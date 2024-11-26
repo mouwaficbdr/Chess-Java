ce684970c81050739840d7f7c2a3bab21571c640
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-11-26 03:47:54 +0100

    Implement movement validation for Roi piece: added `peutBouger` method to check valid moves based on chess rules, ensuring movement is restricted to adjacent squares. Improved code readability and structure in the Roi class.

--- src/main/java/com/mouwafic/piece/Roi.java
+++ src/main/java/com/mouwafic/piece/Roi.java
@@ -6,10 +6,22 @@ public class Roi extends Piece{
     public Roi(int couleur, int col, int ligne) {
         super(couleur, col, ligne);
 
-        if(couleur == PanneauDeJeu.BLANC){
+        if (couleur == PanneauDeJeu.BLANC) {
             image = getImage("/images/w_king");
         } else {
             image = getImage("/images/b_king");
         }
+    }
+
+    public boolean peutBouger(int colCible, int ligneCible) {
+        
+        if (estDansEchiquier(colCible, ligneCible)) {
+            
+            if(Math.abs(colCible - preCol) + Math.abs(ligneCible - preLigne) == 1 || Math.abs(colCible - preCol) * Math.abs(ligneCible - preLigne)  == 1) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
