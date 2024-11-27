c36767e28715bbb66f902dd0a16fdbdb58a556e3
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-11-27 03:46:54 +0100

    Implement movement validation for Cavalier piece: added `peutBouger` method to check valid moves based on chess rules, ensuring movement adheres to knight's unique movement pattern. Improved code readability by restructuring conditional statements and enhancing overall functionality.

--- src/main/java/com/mouwafic/piece/Cavalier.java
+++ src/main/java/com/mouwafic/piece/Cavalier.java
@@ -6,10 +6,23 @@ public class Cavalier extends Piece{
     public Cavalier(int couleur, int col, int ligne) {
         super(couleur, col, ligne);
 
-        if(couleur == PanneauDeJeu.BLANC){
+        if (couleur == PanneauDeJeu.BLANC) {
             image = getImage("/images/w_knight");
         } else {
             image = getImage("/images/b_knight");
         }
     }
+    
+    public boolean peutBouger(int colCible, int ligneCible) {
+
+        if (estDansEchiquier(colCible, ligneCible)) {
+            if (Math.abs(colCible - preCol) * Math.abs(ligneCible - preLigne) == 2) {
+                if (verifieCaseValide(colCible, ligneCible)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
