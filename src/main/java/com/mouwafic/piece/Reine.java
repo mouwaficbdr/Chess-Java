153d745c42ef47679f2feaa5863f6b0b7508dc8b
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-11-27 04:18:19 +0100

    Add movement validation for Reine piece: implemented `peutBouger` method to check valid horizontal, vertical, and diagonal moves based on chess rules. Enhanced code readability and functionality by restructuring conditional statements, ensuring adherence to queen's movement patterns.

--- src/main/java/com/mouwafic/piece/Reine.java
+++ src/main/java/com/mouwafic/piece/Reine.java
@@ -6,10 +6,31 @@ public class Reine extends Piece{
     public Reine(int couleur, int col, int ligne) {
         super(couleur, col, ligne);
 
-        if(couleur == PanneauDeJeu.BLANC){
+        if (couleur == PanneauDeJeu.BLANC) {
             image = getImage("/images/w_queen");
         } else {
             image = getImage("/images/b_queen");
         }
+    }
+    
+    public boolean peutBouger(int colCible, int ligneCible) {
+        if (estDansEchiquier(colCible, ligneCible) && memeCase(colCible, ligneCible) == false) {
+
+            //Vertical et horizontal comme la tour
+            if (colCible == preCol || ligneCible == preLigne) {
+                if (verifieCaseValide(colCible, ligneCible) && pieceSurLigneDroite(colCible, ligneCible) == false) {
+                    return true;
+                }
+            }
+
+            //Diagonal comme le fou
+            if(Math.abs(colCible - preCol) == Math.abs(ligneCible - preLigne)) {
+                if (verifieCaseValide(colCible, ligneCible) && pieceSurDiagonale(colCible, ligneCible) == false) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
     }
 }
