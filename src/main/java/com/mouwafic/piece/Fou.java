9d5a610fa7d109c1daff98ee512b37000a91f173
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-11-27 03:47:01 +0100

    Implement movement validation for Fou piece: added `peutBouger` method to check valid diagonal moves based on chess rules, ensuring movement adheres to the bishop's unique movement pattern. Enhanced code readability by restructuring conditional statements and improving overall functionality.

--- src/main/java/com/mouwafic/piece/Fou.java
+++ src/main/java/com/mouwafic/piece/Fou.java
@@ -6,10 +6,23 @@ public class Fou extends Piece{
     public Fou(int couleur, int col, int ligne) {
         super(couleur, col, ligne);
 
-        if(couleur == PanneauDeJeu.BLANC){
+        if (couleur == PanneauDeJeu.BLANC) {
             image = getImage("/images/w_bishop");
         } else {
             image = getImage("/images/b_bishop");
         }
+    }
+    
+    public boolean peutBouger(int colCible, int ligneCible) {
+
+        if (estDansEchiquier(colCible, ligneCible) && memeCase(colCible, ligneCible) == false) {
+            if(Math.abs(colCible - preCol) == Math.abs(ligneCible - preLigne)) {
+                if (verifieCaseValide(colCible, ligneCible) && pieceSurDiagonale(colCible, ligneCible) == false) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
