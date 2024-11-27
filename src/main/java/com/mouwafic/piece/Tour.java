caebf4e2ab554558c95878436e5cdca28b50ec45
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-11-27 03:47:33 +0100

    Implement movement validation for Tour piece: added `peutBouger` method to check valid horizontal and vertical moves based on chess rules, ensuring adherence to rook's movement pattern. Improved code readability by restructuring conditional statements and enhancing overall functionality.

--- src/main/java/com/mouwafic/piece/Tour.java
+++ src/main/java/com/mouwafic/piece/Tour.java
@@ -6,10 +6,22 @@ public class Tour extends Piece {
     public Tour(int couleur, int col, int ligne) {
         super(couleur, col, ligne);
 
-        if(couleur == PanneauDeJeu.BLANC){
+        if (couleur == PanneauDeJeu.BLANC) {
             image = getImage("/images/w_rook");
         } else {
             image = getImage("/images/b_rook");
         }
+    }
+    
+    public boolean peutBouger(int colCible, int ligneCible) {
+
+        if (estDansEchiquier(colCible, ligneCible) && memeCase(colCible, ligneCible) == false) {
+            if (colCible == preCol || ligneCible == preLigne) {
+                if (verifieCaseValide(colCible, ligneCible) && pieceSurLigneDroite(colCible, ligneCible) == false) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
